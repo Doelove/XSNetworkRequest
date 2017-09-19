6ad2ed3a6bb8f04bdbce64735450bef7ab0a25ab
刘佳斌 <liujiabin9303@163.com>
2017-09-19 16:53:08 +0800

    first commit

---
+++ XSNetworkRequest/XSNetworkRequest.swift
@@ -0,0 +1,13 @@
+//
+//  XSNetworkRequest.swift
+//  XSNetworkRequest
+//
+//  Created by 刘佳斌 on 2017/9/19.
+//  Copyright © 2017年 xiaosi. All rights reserved.
+//
+
+import UIKit
+
+class XSNetworkRequest: NSObject {
+
+}
